14ca97d39725183d33ddb41465e45d61374a56a3
Savan Patel <savanpatelssp@gmail.com>
2026-06-09 11:38:31 +0530

    First successful SP-NET-GRAM build

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,35 +1,15 @@
 plugins {
-    `kotlin-dsl`
-    // kotlin("jvm") version "2.1.0"
-}
-
-gradlePlugin {
-    plugins {
-        register("testGenerator") {
-            id = "test-generator"
-            implementationClass = "com.example.TestGeneratorPlugin"
-        }
-    }
+    kotlin("jvm") version "1.9.22"
 }
 
 repositories {
     google()
     mavenCentral()
-}
-/*
-val checkEmojiKeyboard by tasks.registering(GenerateSchemeTask::class) {
-
-}
-*/
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    compilerOptions {
-        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-    }
+    gradlePluginPortal()
 }
 
 dependencies {
-    compileOnly(gradleApi())
+    implementation(gradleApi())
 
     implementation("com.squareup.moshi:moshi:1.15.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
